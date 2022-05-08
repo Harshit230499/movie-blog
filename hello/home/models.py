0b5ab3fed344c0b7a8391ea4a7323041fb16f7ea
Harshit <Harshitsharma2382@gmail.com>
2022-05-08 18:05:31 +0530

    links added

--- hello/home/models.py
+++ hello/home/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Contact(models.Model):
     email = models.CharField(max_length=200, default='DEFAULT VALUE')
-    password = models.CharField(max_length=120, default='DEFAULT VALUE')
+    password= models.CharField(max_length=120, default='DEFAULT VALUE')
     #date = models.DateTimeField
     
     def __str__(self):
